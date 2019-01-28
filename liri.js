ef4f283d89f39f157c1cd984b2aa7e228ddeefd6
Billy Sterling <44212511+BillySterling@users.noreply.github.com>
2019-01-28 11:31:48 -0500

    ongoing development of code

--- liri.js
+++ liri.js
@@ -1,13 +1,16 @@
 // required npm packages
+require("dotenv").config();
 var axios = require("axios");
+var Spotify = require('node-spotify-api');
 var moment = require('moment');
 moment().format();
-require("dotenv").config();
+// file system builtin
+let fs = require('fs'); 
 
 // Add the code required to import the `keys.js` file and store it in a variable.
 var keys = require("./keys.js");
-//var spotify = new Spotify(keys.spotify);
-//axious npm fa-pulse
+var spotify = new Spotify(keys.spotify);
+console.log(spotify);
 
 
 var option = process.argv[2];
@@ -22,9 +25,14 @@ if (option === "concert-this") {
 function concertThis(key) {
     console.log(key);
     var concertThisURL = "https://rest.bandsintown.com/artists/" + key + "/events?app_id=codingbootcamp";
-    console.log(concertThisURL);
+    //console.log(concertThisURL);
 axios.get(concertThisURL).then(
   function(response) {
-    console.log(response.data);
+    //console.log(response.data[0]);
+    console.log("concert-this response: ");
+    console.log("Venue Name: " + response.data[0].venue.name);
+    console.log("Venue Location: " + response.data[0].venue.city + " " + response.data[0].venue.region);
+    var concertDate = moment(response.data[0].datetime).format('MM/DD/YYYY');
+    console.log("Date: " + concertDate);
   }
-)};
+  )};
