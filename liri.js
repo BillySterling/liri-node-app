6bfa62a2fabaf56e85e01e32a373745a6cd2f171
Billy Sterling <44212511+BillySterling@users.noreply.github.com>
2019-01-29 10:54:00 -0500

    coded the 'do-what-it-says' section

--- liri.js
+++ liri.js
@@ -5,7 +5,7 @@ var Spotify = require('node-spotify-api');
 var moment = require('moment');
 moment().format();
 // file system builtin
-let fs = require('fs'); 
+var fs = require('fs'); 
 
 // Add the code required to import the `keys.js` file and store it in a variable.
 var keys = require("./keys.js");
@@ -32,22 +32,26 @@ for (var i = 3; i < nodeArgs.length; i++) {
 
 console.log(searchVal);
 
-switch(option) {
-    case "concert-this":
-        concertThis(searchVal);
-        break;
-    case "spotify-this-song":
-        spotifyThisSong(searchVal);
-        break;
-    case "movie-this":
-        movieThis(searchVal);
-        break;
-    case "do-what-it-says":
-        doWhatItSays(searchVal);
-        break;
-    default:
-        console.log("*** INVALID INPUT***");
-    };
+getInput(option,searchVal);
+
+function getInput(option,searchVal) {
+    switch(option) {
+        case "concert-this":
+            concertThis(searchVal);
+            break;
+        case "spotify-this-song":
+            spotifyThisSong(searchVal);
+            break;
+        case "movie-this":
+            movieThis(searchVal);
+            break;
+        case "do-what-it-says":
+            doWhatItSays(searchVal);
+            break;
+        default:
+            console.log("*** INVALID INPUT***");
+        };
+};
 
 function concertThis(searchVal) {
     var concertThisURL = "https://rest.bandsintown.com/artists/" + searchVal + "/events?app_id=codingbootcamp";
@@ -58,7 +62,6 @@ function concertThis(searchVal) {
         //debugger;
         concertData = response.data;
         console.log("concert-this response for :"  + searchVal);
-        debugger;
         concertData.forEach(concert => {
             console.log("Venue Name: " + concert.venue.name);
             console.log("Venue Location: " + concert.venue.city + " " + concert.venue.region);
@@ -114,3 +117,23 @@ function movieThis(searchVal) {
     }
     );
 }
+
+function doWhatItSays() {
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        // If the code experiences any errors it will log the error to the console.
+        if (error) {
+        return console.log(error);
+        }    
+        // We will then print the contents of data
+        console.log(data);
+        // create array from file
+        var dataArr = data.split(',');   
+        //console.log("data: " + data);
+        option = dataArr[0];
+        searchVal = dataArr[1];
+        // remove extra double quotes
+        searchVal = searchVal.replace(/\"/g, '');
+        // call the main input process with the values from the file
+        getInput(option,searchVal);
+    });
+};
